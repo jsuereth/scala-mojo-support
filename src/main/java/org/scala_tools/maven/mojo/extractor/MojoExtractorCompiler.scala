fbfd48a3a491f6456eb11b513f87d82939846f9a
jsuereth <Joshua.Suereth@gmail.com>
2009-01-10 23:01:58 -0500

    Initial class + annotation processing

--- src/main/java/org/scala_tools/maven/mojo/extractor/MojoExtractorCompiler.scala
+++ src/main/java/org/scala_tools/maven/mojo/extractor/MojoExtractorCompiler.scala
@@ -52,51 +52,83 @@ class MojoExtractorCompiler(project : MavenProject) extends MavenProjectTools{
 
 import scala.tools.nsc.ast._
 import scala.tools.nsc.symtab.{Flags, SymbolTable}
+import org.scala_tools.maven.mojo.annotations._
 trait MojoAnnotationExtractor extends CompilationUnits {
   self: Global =>
     
-  
+  /** Pulls all mojo classes out of the body of source code. */
   def parseCompilationUnitBody(body : Tree) = {
-    
-    
-    def parseClass(tree : ClassDef, pkgName : String) = {
-      val info = new MojoClassInfo(tree.name.toString)
-      
-      //TODO - Rip annotations from the class
-      
-      for(annotation <- tree.mods.annotations) {
-        info.annotation(annotation.tpe.safeToString)
+    /** Parses a list of annotations into a list of MojoAnnotationInfo classes */
+    def parseAnnotations(annotations : List[Annotation]) = {
+      for {
+        annotation <- annotations 
+        Annotation(constr, _) <- annotation
+      } yield {
+        // TODO - In the future the second arg to Annotation may be needed... (as it's what's inside the
+        //anonymous instantiation of an annotatioin, i.e. @xyz {}
+        //TODO - Do we need to match on constructor?
+        val argVals = for { 
+          Apply(_, args) <- constr
+          arg <- args
+          argVal <- extractStaticValue(arg)
+        } yield argVal
+        new MojoAnnotationInfo(annotation.tpe.safeToString, argVals.toList)
       }
+    }
+    
+    /** Pulls out mojo information froma mojo class */
+    def parseMojoClass(mojoClass : ClassDef) = {
       
+      Console.println(" Found mojo class: " + mojoClass.name.toString)
+      //TODO - Pull out *FULL* Name
+      val info = new MojoClassInfo(mojoClass.name.toString)
+      
+      //TODO - Rip annotations from the class and add to MojoClassInfo
+      parseAnnotations(mojoClass.mods.annotations).foreach(info.annotation(_))
+      //TODO - Rip out annotated Var methods
+      for { node <- mojoClass.impl } {
+        
+      }
+      //TODO - Parse Parent classes
       info
     }
-    import scala.collection.mutable
-    def parseClasses(body : Tree, pkgName : String, infos : mutable.ListBuffer[MojoClassInfo]) : Unit = {
-      body match {
-         case tree : ClassDef =>
-           infos.append(parseClass(tree, pkgName))
-         case _ => //IGnore
-      }
-    }
-    
-    //Pull out mojo class information
-    val classInfos = new mutable.ListBuffer[MojoClassInfo]    
-    for(node <- body) {
-      parseClasses(node, "", classInfos)
-    }
-    classInfos
+    //Find mojo classes
+    for { classDef @ ClassDef(mods,name,params, impl) <- body
+          annotation <- mods.annotations
+          if annotation.tpe.safeToString == classOf[goal].getName
+    } yield parseMojoClass(classDef)    
   }
   
+  /** Attempts to pull a static value from the given tree.
+   * 
+   * @returns
+   *         Some(value) if a value is found, None otherwise
+   */
+  def extractStaticValue(tree : Tree) = tree match {
+    case Literal(c) => Some(extractConstantValue(c)) 
+    case _ => None
+  }
+  /**
+   * Extracts a constant variable's runtime value
+   */
+  def extractConstantValue(c : Constant) = {
+    //TODO - handle other values
+    c.stringValue
+  }
 }
 
 //Class information string
 class MojoClassInfo(val name : String) {
-  private[this] var annotations : List[String] = Nil
-  def annotation(name : String) {
+  private[this] var annotations : List[MojoAnnotationInfo] = Nil
+  def annotation(name : MojoAnnotationInfo) {
     annotations = name :: annotations
   }
   
   def getAnnotations() = annotations
   
   override def toString = name + " - " + annotations.mkString("(",",",")")
+}
+
+class MojoAnnotationInfo(val name : String, val args : List[String]) {
+  override def toString = name + args.mkString("(",",",")")
 }
